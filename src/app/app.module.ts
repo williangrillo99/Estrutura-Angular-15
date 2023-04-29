32fb34dc50ec50554500ae8f30afa92bbcc4acc9
Willian Grillo <williangrillo@outlook.com.br>
2023-04-29 18:32:40 -0300

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent, RegisterComponent } from './account';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+    BrowserModule, 
+    AppRoutingModule],
+
+    declarations:[
+        AppComponent,
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent
+        
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
